e3842e246c3d372e0b401476721f42bb25804f54
Aaryan2324 <129336503+Aaryan2324@users.noreply.github.com>
2024-10-10 07:12:17 +0530

    databse connection

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grids";
+import { getMeals } from "@/lib/meals";
 
-export default function MealsPage() {
+export default async function MealsPage() {
+  const meals = await getMeals();
+
   return (
     <>
       <header className={classes.header}></header>
@@ -17,7 +20,7 @@ export default function MealsPage() {
         <Link href="/meals/share">Share Your Favorite Recipe</Link>
       </p>
       <main className={classes.main}>
-        <MealsGrid meals={[]} />
+        <MealsGrid meals={meals} />
       </main>
     </>
   );
